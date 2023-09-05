706d897b9a94d577b62234df1643d2501f640637
Ketul <ketulm8@gmail.com>
2023-09-05 23:07:14 +0530

    Get current latitude and longitude of the user'sdevice

---
+++ lib/home_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Location location = Location();
+  late Future<LocationData?> _currentLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentLocation = getCurrentLocation();
+  }
+
+  Future<LocationData?> getCurrentLocation() async {
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return null;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+
+    return await location.getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: FutureBuilder(
+        future: _currentLocation,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              debugPrint(snapshot.data!.heading.toString());
+              return Text(snapshot.data!.altitude.toString());
+            }
+            return Text(snapshot.error.toString());
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      )),
+    );
+  }
+}
